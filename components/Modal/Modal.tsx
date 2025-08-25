20418a4b1dd9abda017fd156e3b22b7e4f77f195
KateChmil <chmilkatecat@gmail.com>
2025-08-25 21:07:54 +0300

    updated

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
-import css from "./NotePreviewModal.module.css";
+import css from "./Modal.module.css";
 
 
 interface ModalProps{
